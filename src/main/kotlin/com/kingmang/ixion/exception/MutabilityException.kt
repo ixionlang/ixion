150fa77c9062a4123471507d2067f8700ba08500
k1ngmang <kingmangapp@gmail.com>
2026-05-28 12:36:18 +0300

    upd exceptions

--- src/main/kotlin/com/kingmang/ixion/exception/MutabilityException.kt
+++ src/main/kotlin/com/kingmang/ixion/exception/MutabilityException.kt
@@ -2,6 +2,6 @@ package com.kingmang.ixion.exception
 
 class MutabilityException : IxException(
     14,
-    "Variable `{0}` is immutable and cannot receive assignment.",
-    "Declare a variable with the `mut` keyword to allow mutability."
+    "Variable `{0}` is constant and cannot receive assignment.",
+    "Declare a variable with the `var` keyword to allow mutability."
 )
